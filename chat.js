9a8b62db68877a63446b381b50473ac645a26441
Ilya <ilabutaev@gmail.com>
2020-06-20 18:07:18 +0300

    выф

--- chat.js
+++ chat.js
@@ -7,8 +7,16 @@ window.onload = function(){
 }
 
 
-let btnMobChat = document.querySelector('.chats_mobile');
+
+let btnMobChat = document.querySelector('.chats_mobile'),
+    filterDropdown = document.querySelector('.filter_dropdown');
+
 btnMobChat.addEventListener('click', (e)=>{
     chatsMobileAdaptive.classList.toggle('sidebar_mobile-seek')
     chatsMobileAdaptive.classList.toggle('sidebar_mobile-hide')
+});
+
+filterBtn.addEventListener('click', (e)=>{
+    filterDropdown.classList.toggle('filter_dropdown-show');
+    filterDropdown.classList.toggle('filter_dropdown-hide')
 });
